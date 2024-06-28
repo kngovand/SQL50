95a15a14b4999db3b475c37d5c2901b539c62076
Kevin Duc <kngovand@gmail.com>
2024-06-28 13:54:33 -0600

    15 and 16

--- sql50_15.sql
+++ sql50_15.sql
@@ -1,2 +1,6 @@
-/* Write your T-SQL query statement below */
-select A.product_id, avg() as average_price
+SELECT p.product_id, IFNULL(round(SUM(p.price*u.units)/sum(u.units),2),0) as average_price
+FROM Prices p 
+LEFT JOIN UnitsSold u
+ON p.product_id = u.product_id AND 
+u.purchase_date BETWEEN p.Start_date and p.end_date
+GROUP BY p.product_id
--- sql50_16.sql
+++ sql50_16.sql
@@ -1,1 +1,9 @@
---to do
+SELECT
+    T1.project_id,
+    ROUND(AVG(CAST(experience_years aS DECIMAL(10,2))) ,2) [average_years]
+FROM
+    Project T1 
+INNER JOIN
+    Employee T2 ON T1.employee_id = T2.employee_id
+GROUP BY
+    T1.project_id
