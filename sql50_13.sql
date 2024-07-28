2efc9ce82a7787af4cd2375c58410677289f6991
Kevin Duc <kngovand@gmail.com>
2024-07-28 13:38:52 -0600

    Revisiting 13, added 17

--- sql50_13.sql
+++ sql50_13.sql
@@ -1,1 +1,11 @@
---to do
+SELECT
+  s.user_id,
+  ROUND(SUM(CASE
+    WHEN c.action = 'confirmed' THEN 1
+    WHEN s.time_stamp IS NULL THEN 0.00
+    ELSE 0
+  END) / COUNT(*), 2) AS confirmation_rate
+FROM signups s
+LEFT JOIN confirmations c
+  ON s.user_id = c.user_id
+GROUP BY s.user_id
